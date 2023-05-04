fbf1c9f245be43bb97077eb6c7b5ef9974a46b14
as858291 <as858291@gmail.com>
2023-05-05 10:58:50 +1200

    Created a new Solution

---
+++ Exercise1_inheritance/Exercise1_inheritance/AnimalManager.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercise1_inheritance
+{
+    internal class AnimalManager:Animal
+    {
+        public void AddAnimal(string name)
+        {
+            animalList.Add(name);
+        }
+
+        public void DisplayAnimal()
+        {
+            foreach (string animal in animalList)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+
+        public void RemoveAnimal(string name)
+        {
+            animalList.Remove(name);
+        }
+    }
+}
---
+++ Exercise1_inheritance/Exercise1_inheritance/Dog.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercise1_inheritance
+{
+    internal class Dog: Animal
+    {
+        public Dog(string name)
+        {
+            AnimalName = name;
+            Sound = "Woof Woof";
+        }
+
+        public void DogBarkingReason()
+        {
+            List<string> barkingReasonList = new List<string> { "greeting barking", "Frustration-Induced Barking" };
+
+            foreach (var variable in barkingReasonList)
+            {
+                Console.WriteLine(variable);
+
+            }
+
+
+        }
+
+    }
+}
