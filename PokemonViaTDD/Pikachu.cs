167b8b7fc5c91d49cb70da001c7a997e76192829
as858291@gmail.com <as858291@gmail.com>
2023-08-31 19:28:52 +1200

    Added Pikachu Sotion

---
+++ PokemonViaTDD/Pikachu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonViaTDD
+{
+     class Pikachu 
+    {
+       
+public Attacktype Attack()
+{
+            return new Attacktype("Thunderstorm",10);
+}
+
+       
+    }
+
+
+     class Attacktype
+     {
+         public string Type { get; set; }
+         public int Power { get; set; }
+        public Attacktype(string type, int power)
+         {
+             Type=type;
+             Power=power;
+
+         }
+
+     
+
+
+    }
+
+    
+}
---
+++ PokemonViaTDD/PikachuTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PokemonViaTDD;
+
+namespace PokemonViaTDD
+{
+    [TestClass]
+    public class PikachuTests
+    {
+        [TestMethod]
+        public void Attack_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var pikachu = new Pikachu();
+
+            // Act
+            var result = pikachu.Attack();
+
+            // Assert
+            Assert.AreEqual(result.Type, "Thunderstorm", "match");
+        }
+    }
+}
